2aac90f5f6d4d903093068219646ca08f2001f8d
marlinejohn <marlinejohn@gmail.com>
2022-09-19 23:31:13 +0200

    Added Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="Container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <main>
+            <Dictionary />
+          </main>
+        </header>
+        <footer className="App-footer">
+          <small>Coded by Marline John</small>
+        </footer>
+      </div>
     </div>
   );
 }
