87e46238bb529228c485966e5c1c6d28b135b4d6
marlinejohn <marlinejohn@gmail.com>
2022-09-21 23:44:45 +0200

    Added more components to render  word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function Search(event) {
@@ -25,6 +27,8 @@ export default function Dictionary() {
       <form onSubmit={Search}>
         <input type="Search" onChange={handleKeywordChange}></input>
       </form>
+
+      <Results results={results} />
     </div>
   );
 }
